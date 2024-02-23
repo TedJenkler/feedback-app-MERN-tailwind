c1701fd0e3519bc8a8ca70b276fc42b67a88a30d
TedJenkler <Tedjenkler@gmail.com>
2024-02-23 04:53:12 -0200

    redux tkk installed and setup

---
+++ src/features/state/stateSlice.jsx
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+import data from "../../../data.json"
+
+const initialState = {
+    data
+}
+
+export const stateSlice = createSlice({
+    name: 'state',
+    initialState,
+    reducers: {
+        test: (state) => {
+            state
+        }
+    }
+})
+
+export const { test } = stateSlice.actions
+
+export default stateSlice.reducer
