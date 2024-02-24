252f7860ac7140b7997a4d984afd9e93c5293bdb
TedJenkler <Tedjenkler@gmail.com>
2024-02-24 03:40:26 -0200

    AddNewFeedback useState added

--- src/components/AddNewFeedback.jsx
+++ src/components/AddNewFeedback.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import arrowleft from "../assets/arrowleft.png"
 
 function AddNewFeedback( {setToggleAdd, toggleAdd} ) {
+    const [title, setTitle] = useState("")
+    const [select, setSelect] = useState("Feature")
+    const [detail, setDetail] = useState("")
+    console.log(title)
+    console.log(select)
+    console.log(detail)
   return (
-    <main className='absolute h-full w-full bg-grey-white2 top-0 bottom-0 left-0 right-0 px-6 py-10'>
-    <button onClick={(e) => setToggleAdd(!toggleAdd)} className='flex items-center gap-1'>
+    <main className='absolute h-full w-full bg-grey-white2 top-0 left-0 right-0 px-6 pt-10'>
+    <button onClick={(e) => setToggleAdd(!toggleAdd)} className='flex items-center gap-1 mb-8'>
         <img className='w-1 h-2' src={arrowleft} alt='arrowback' />
         <p className='text-grey text-sm font-bold'>Go Back</p>
     </button>
+    <div className='flex flex-col text-black mt-5 py-11 px-6 bg-white'>
+        <h1 className='text-lg text-blue font-bold mb-6'>Create New Feedback</h1>
+        <h2 className='text-sm text-blue font-bold mb-1'>Feedback Title</h2>
+        <p className='text-sm text-grey font-normal mb-4'>Add a short, descriptive headline</p>
+        <textarea onChange={(e) => setTitle(e.target.value)} value={title} className='bg-grey-white2 h-12 mb-6'></textarea>
+        <h2 className='text-sm text-blue font-bold mb-1'>Category</h2>
+        <p className='text-sm text-grey font-normal mb-4'>Choose a category for your feedback</p>
+        <select onChange={(e) => {setSelect(e.target.value)}} className='bg-grey-white2 h-12 mb-6'>
+            <option value={"Feature"}>Feature</option>
+            <option value={"UI"}>UI</option>
+            <option value={"UX"}>UX</option>
+            <option value={"Enchantment"}>Enchantment</option>
+            <option value={"Bug"}>Bug</option>
+        </select>
+        <h2 className='text-sm text-blue font-bold mb-1'>Feedback Detail</h2>
+        <p className='text-sm text-grey font-normal mb-4'>Include any specific comments on what should be improved, added, etc.</p>
+        <textarea onChange={(e) => setDetail(e.target.value)} value={detail} className='bg-grey-white2 h-32 mb-10'></textarea>
+        <button className='bg-purple text-white mb-4 py-2 rounded-xl'>Add Feedback</button>
+        <button className='bg-blue text-white py-2 rounded-xl'>Cancel</button>
+    </div>
     </main>
   )
 }
