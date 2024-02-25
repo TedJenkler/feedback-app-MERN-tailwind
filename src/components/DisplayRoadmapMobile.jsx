1e2fb7ae09cfbe56fd28aba6015d58c07ccceaaa
TedJenkler <Tedjenkler@gmail.com>
2024-02-25 07:18:19 -0200

    roadmap mobile done

--- src/components/DisplayRoadmapMobile.jsx
+++ src/components/DisplayRoadmapMobile.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import arrowup from "../assets/arrowup.png";
 import comment from "../assets/comment.png";
+import orange from "../assets/orange.png"
+import purple from "../assets/purple.png"
+import blue from "../assets/blue.png"
 
 function DisplayRoadmapMobile( {selectedRoadmap} ) {
   const state = useSelector((state) => state.state.data.productRequests)
@@ -19,19 +22,45 @@ function DisplayRoadmapMobile( {selectedRoadmap} ) {
     }
   }
 
+  const plannedCount = state.reduce((acc, currentValue) => {
+    if (currentValue.status === "planned") {
+        return acc + 1;
+    }
+    return acc;
+}, 0);
+
+const inProgressCount = state.reduce((acc, currentValue) => {
+    if (currentValue.status === "in-progress") {
+        return acc + 1;
+    }
+    return acc;
+}, 0);
+
+const liveCount = state.reduce((acc, currentValue) => {
+    if (currentValue.status === "live") {
+        return acc + 1;
+    }
+    return acc;
+}, 0);
+
   useEffect(() => {
     FilterRoadmapFunction()
   },[selectedRoadmap])
   return (
     <main className='bg-grey-white2 p-6'>
-      <h1>In-Progress (3)</h1>
-      <p>Features currently being developed</p>
+      <h1 className='text-xl text-blue font-bold mb-1'>In-Progress ({selectedRoadmap === "planned" ? plannedCount : selectedRoadmap === "in-progress" ? inProgressCount : liveCount })</h1>
+      {selectedRoadmap === "planned" ? <p className='mb-6 text-sm text-grey font-normal'>Ideas prioritized for research</p>: null}
+      {selectedRoadmap === "in-progress" ? <p className='mb-6 text-sm text-grey font-normal'>Currently being developed</p>: null}
+      {selectedRoadmap === "live" ? <p className='mb-6 text-sm text-grey font-normal'>Released features</p>: null}
       {roadmap.map((feedback) => {
-        console.log(feedback)
         return (
           <div className='bg-white mb-6 p-6 rounded-xl border-t-8 border-purple'>
-            <h2>{feedback.title}</h2>
-            <p>{feedback.description}</p>
+
+            {selectedRoadmap === "planned" ? <div className='flex items-center gap-2 mb-4'><img className='h-2 w-2' src={orange} alt="orange oval" /><p>Planned</p></div> : null}
+            {selectedRoadmap === "in-progress" ? <div className='flex items-center gap-2 mb-4'><img className='h-2 w-2' src={purple} alt="purple oval" /><p>Progress</p></div> : null}
+            {selectedRoadmap === "live" ? <div className='flex items-center gap-2 mb-4'><img className='h-2 w-2' src={blue} alt="blue oval" /><p>Live</p></div> : null}
+            <h2 className='text-sm font-bold text-blue mb-2'>{feedback.title}</h2>
+            <p className='text-grey text-sm font-normal mb-2'>{feedback.description}</p>
             <div className='items-center justify-center bg-grey-white py-1 px-4 rounded-xl text-sm inline-block mb-4'>
               <p className='text-strong-blue font-semibold'>{feedback.category[0].toLocaleUpperCase() + feedback.category.substr(1)}</p>
             </div>
