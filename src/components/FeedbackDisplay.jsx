630100b67bd8fd2db46a8474b7f0b9cf13c193ef
TedJenkler <Tedjenkler@gmail.com>
2024-02-26 04:32:11 -0200

    special stylying for tablet

--- src/components/FeedbackDisplay.jsx
+++ src/components/FeedbackDisplay.jsx
@@ -38,21 +38,31 @@ function FeedbackDisplay( {toggleView, setToggleView, selectedFeedback, setSelec
     return (
         <main className='bg-grey-white py-8 min-h-full'>
             {filteredAndSortedRequests.map((value) => (
-                <div onClick={(e) => handleListClick(value.id)} className='bg-white mx-6 mb-4 rounded-xl p-6' key={value.id}>
-                    <p className='text-sm font-bold text-blue mb-2'>{value.title}</p>
-                    <p className='text-grey text-sm font-normal mb-2'>{value.description}</p>
-                    <div className='items-center justify-center bg-grey-white py-2 px-4 rounded-xl text-sm inline-block mb-4'>
-                        <p className='text-strong-blue font-semibold'>{value.category[0].toLocaleUpperCase() + value.category.substr(1)}</p>
+                <div onClick={(e) => handleListClick(value.id)} className='bg-white mx-6 mb-4 rounded-xl p-6 md:mx-10 md:flex md:flex-row-reverse md:justify-between' key={value.id}>
+                    <button className='hidden absolute items-center gap-1 md:flex md:relative'>
+                            <img className='h-4 w-5' src={comment} alt='comments' />
+                            <p>{value.comments ? value.comments.length : 0}</p>
+                    </button>
+                    <div className='md:flex md:flex-row-reverse md:gap-10'>
+                    <div className=''>
+                        <p className='text-sm font-bold text-blue mb-2'>{value.title}</p>
+                        <p className='text-grey text-sm font-normal mb-2'>{value.description}</p>
+                        <div className='items-center justify-center bg-grey-white py-2 px-4 rounded-xl text-sm inline-block mb-4'>
+                            <p className='text-strong-blue font-semibold'>{value.category[0].toLocaleUpperCase() + value.category.substr(1)}</p>
+                        </div>
                     </div>
                     <div className='flex justify-between'>
-                        <button className='flex bg-grey-white items-center gap-2 py-2 px-3 rounded-xl'>
+                    <div className='flex justify-between'>
+                        <button className='flex bg-grey-white items-center gap-2 py-2 px-3 rounded-xl md:flex-col md:h-12 md:w-10 md:p-2'>
                             <img className='w-2 h-1' src={arrowup} alt='arrowup' />
                             <p className='text-sm text-blue font-bold'>{value.upvotes}</p>
                         </button>
-                        <button className='flex items-center gap-1'>
+                    </div>
+                        <button className='flex items-center gap-1 md:hidden'>
                             <img className='h-4 w-5' src={comment} alt='comments' />
                             <p>{value.comments ? value.comments.length : 0}</p>
                         </button>
+                    </div>
                     </div>
                 </div>
             ))}
--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { sort } from '../features/state/stateSlice'
 import { Link } from 'react-router-dom'
+import lightbulb from "../assets/lightbulb.png"
 
 function Sort() {
     const [sortBy, setSortBy] = useState("Most Upvotes")
+    const state = useSelector((state) => state.state.data.productRequests)
     const dispatch = useDispatch()
+
+    const suggestionCount = state.reduce((acc, currentValue) => {
+        if (currentValue.status === "suggestion") {
+            return acc + 1;
+        }
+        return acc;
+    }, 0);
+
     useEffect(() => {
         dispatch(sort(sortBy))
     }, [sortBy])
   return (
-    <div className='flex justify-between h-14 items-center text-white px-6 bg-dark-blue text-sm'>
+    <div className='flex justify-between h-14 items-center text-white px-6 bg-dark-blue text-sm md:mx-10 md:rounded-xl md:h-20'>
         <div className='flex items-center'>
+            <div className='hidden absolute md:flex md:relative md:gap-2 md:items-center mr-10'>
+                <img src={lightbulb} alt='lightbulb' />
+                <p className='text-lg font-bold text-white'>{suggestionCount} Suggestions</p>
+            </div>
             <label className='font-normal whitespace-nowrap'>Sort by : </label>
             <select className='bg-dark-blue font-bold w-32 mr-4' onChange={(e) => {setSortBy(e.target.value)}}>
                 <option value="Most Upvotes"> Most Upvotes</option>
