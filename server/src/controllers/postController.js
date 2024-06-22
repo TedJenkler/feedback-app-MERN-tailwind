fd0067459a958690653d842dd4c0b02ac323cfa6
Ted Jenkler <tedjenkler@gmail.com>
2024-06-22 15:31:07 +0200

    get All posts and getPostById added

--- server/src/controllers/postController.js
+++ server/src/controllers/postController.js
@@ -2,6 +2,36 @@ const Post = require('../models/postSchema');
 const User = require('../models/userSchema');
 const Category = require('../models/categorySchema');
 
+exports.getAllPost = async (req, res) => {
+    try {
+        const posts = await Post.find();
+        if(posts.length === 0) {
+            return res.status(404).json({ message: 'No posts found' });
+        }
+
+        res.status(200).json({ message: 'Posts fetched successfully', posts });
+    }catch (error) {
+        console.error('Failed fetching posts', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+exports.getPostById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const post = await Post.findById(id);
+        if(!post) {
+            return res.status(404).json({ message: 'No post found' });
+        }
+
+        res.status(200).json({ message: 'Post fetched successfully', post });
+    }catch (error) {
+        console.error('Failed fetching post', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
 exports.addPost = async (req, res) => {
     try {
         const { title, description, category, user } = req.body;
--- server/src/routes/post.js
+++ server/src/routes/post.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 
+router.get('/', postController.getAllPost);
+router.get('/:id', postController.getPostById);
 router.post('/add', postController.addPost);
 
 module.exports = router
