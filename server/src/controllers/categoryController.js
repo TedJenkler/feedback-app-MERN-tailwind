b3d6264a01965371385d981a4ca196f9a54c0b6c
Ted Jenkler <tedjenkler@gmail.com>
2024-06-22 13:21:24 +0200

    getAll, get by name and id added

--- server/src/controllers/categoryController.js
+++ server/src/controllers/categoryController.js
@@ -1,5 +1,51 @@
 const Category = require('../models/categorySchema');
 
+exports.getAllCategories = async (req, res) => {
+    try{
+        const categories = await Category.find();
+        if (categories.length === 0) {
+            return res.status(404).json({ message: 'No categories found' });
+        }
+
+        res.status(200).json({ message: 'Categories fetched successfully', categories });
+    }catch (error) {
+        console.error('Error fetching categories', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+exports.getCategoryById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const category = await Category.findById(id);
+        if (!category) {
+            return res.status(404).json({ message: 'Category not found' });
+        }
+
+        res.status(200).json({ message: 'Category fetched successfully', category });
+    }catch (error) {
+        console.error('Error fetching category', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+exports.getCategoryByName = async (req, res) => {
+    try{
+        const { name } = req.params;
+
+        const category = await Category.findOne({ name: name });
+        if(!category) {
+            res.status(404).json({ message: 'Category not found' });
+        }
+
+        res.status(200).json({ message: 'Category fetched successfully', category });
+    }catch (error) {
+        console.error('Error fetching category', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
 exports.addCategory = async (req, res) => {
     try {
         const { name } = req.body;
@@ -19,6 +65,6 @@ exports.addCategory = async (req, res) => {
         res.status(200).json({ message: 'Category added successfully', category });
     }catch (error) {
         console.error('Error adding category', error);
-        res.status(500).json({ message: 'Internal Server Error' })
+        res.status(500).json({ message: 'Internal Server Error' });
     }
 };
--- server/src/routes/category.js
+++ server/src/routes/category.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController');
 
+router.get('/', categoryController.getAllCategories);
+router.get('/:id', categoryController.getCategoryById);
+router.get('/name/:name', categoryController.getCategoryByName);
 router.post('/add', categoryController.addCategory);
 
 module.exports = router
