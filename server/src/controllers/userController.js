7da9893f1dfb91a23e4bad4be51ed2a784e29b2e
Ted Jenkler <tedjenkler@gmail.com>
2024-06-22 11:47:44 +0200

    get on id, username and email added

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,8 +1,63 @@
 const User = require("../models/userSchema");
 const bcrypt = require('bcrypt');
 
-exports.getAllUsers = (req, res) => {
-    res.send('Get all users');
+exports.getAllUsers = async (req, res) => {
+    try{
+        const users = await User.find();
+
+        res.status(200).json({ message: 'Users fetched succesfully', users});
+    }catch (error) {
+        console.error('Failed to get Users', error);
+        res.status(500).json({ message: 'Internal Server Error'});
+    }
+};
+
+exports.getOneUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        };
+
+        res.status(200).json({ message: 'User fetched succesfully', user})
+    }catch (error) {
+        console.error('Failed fetching User by id', error);
+        res.status(500).json({ message: 'Internal Server Error'});
+    }
+};
+
+exports.getOneUserByEmail = async (req, res) => {
+    try {
+        const { email } = req.params;
+
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        };
+
+        res.status(200).json({ message: 'User fetched succesfully', user})
+    }catch (error) {
+        console.error('Failed fetching User by email', error);
+        res.status(500).json({ message: 'Internal Server Error'});
+    }
+};
+
+exports.getOneUserByUsername = async (req, res) => {
+    try {
+        const { username } = req.params;
+
+        const user = await User.findOne({ username: username });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        };
+
+        res.status(200).json({ message: 'User fetched succesfully', user});
+    }catch (error) {
+        console.error('Failed fetching User by username', error);
+        res.status(500).json({ message: 'Internal Server Error'});
+    }
 };
 
 exports.registerUser = async (req, res) => {
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+/* User */
+
 router.get('/', userController.getAllUsers);
+router.get('/:id', userController.getOneUserById);
+router.get('/username/:username', userController.getOneUserByUsername);
+router.get('/email/:email', userController.getOneUserByEmail);
 router.post('/register', userController.registerUser);
 
 module.exports = router;
