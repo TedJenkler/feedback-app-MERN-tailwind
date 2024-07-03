a2527c72bed3ea09b437de5d23485876cafcc45e
Ted Jenkler <tedjenkler@gmail.com>
2024-07-03 08:08:28 +0200

    fixed scroll bugfix and improved code

--- server/client/src/components/MenuMobile.jsx
+++ server/client/src/components/MenuMobile.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Filter from "../components/Filter";
 import Roadmap from './Roadmap';
 
 function MenuMobile({ toggleMenu }) {
+    useEffect(() => {
+        if (toggleMenu) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+        return () => {
+            document.body.style.overflow = 'auto';
+        };
+    }, [toggleMenu]);
+
     return (
-        <div className={toggleMenu ? 'absolute w-[72.27%] h-full bg-grey-white2 top-[4.5rem] right-0 z-50' : 'absolute hidden w-3/4 h-full bg-grey-white2 top-20 right-0 z-50'}>
-            {/* Render the Filter component */}
+        <div className={`absolute ${toggleMenu ? 'w-[72.27%] overflow-y-auto' : 'hidden'} h-full bg-grey-white2 top-[4.5rem] right-0 z-50`}>
             <Filter />
-            {/* Render the Roadmap component */}
             <Roadmap />
         </div>
     );
